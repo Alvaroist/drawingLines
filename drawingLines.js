0cdc1838f2217e45715e0234401e3e661dd01ed3
Alvaro Padilla <alvaro.padilla2004@gmail.com>
2022-06-14 22:45:25 -0400

    adaptado para telefonos

--- drawingLines.js
+++ drawingLines.js
@@ -6,7 +6,13 @@ var arrowKey={ /*variable declaration area*/
 };
 
 var aCanvas = document.getElementById("areaCanvas");
-var resetButton = document.getElementById("resetButton");
+var resetButton = document.getElementsByName("resetButton");
+
+var arrowUp = document.getElementsByName("arrowUp");
+var arrowDown = document.getElementsByName("arrowDown");
+var arrowLeft = document.getElementsByName("arrowLeft");
+var arrowRight = document.getElementsByName("arrowRight");
+
 var area = aCanvas.getContext("2d");
 var x1 = 150;/*centro del lienzo*/
 var y1 = 150;
@@ -17,10 +23,9 @@ var color="red";
 
 drawOneLine(color,x1-1,y1-1,x2+1,y2+1,area);/*indica donde esta el cursor*/
 document.addEventListener("keyup",keyPressed);
-document.addEventListener("click",reset);
+document.addEventListener("click",oneclick);
 
-function drawOneLine(color,x1,y1,x2,y2,panel)
-{
+function drawOneLine(color,x1,y1,x2,y2,panel){
   panel.beginPath();
   panel.strongStyle=color;
   panel.lineWidth=4;
@@ -30,18 +35,73 @@ function drawOneLine(color,x1,y1,x2,y2,panel)
   panel.closePath();
 }
 
-function reset()
-{
-  area.clearRect(0,0,300,300);
-  drawOneLine(color,x1-1,y1-1,x2+1,y2+1,area);
+function oneclick(clicked){
+  var distance=10;
+  console.log(clicked.target.name);
+
+  switch (clicked.target.name){
+    case "arrowUp":
+      if (y2>=10){
+        y2 = y2 - distance;
+        drawOneLine(color, x1, y1, x2, y2, area);
+        x1=x2;
+        y1=y2;
+        break;
+      }else{
+        x1=x2;
+        y1=y2;
+        break;
+      }
+    case "arrowDown":
+      if(y2<=290){
+        y2 = y2 + distance;
+        drawOneLine(color, x1, y1, x2, y2, area);
+        x1=x2;
+        y1=y2;
+        break;
+      }
+      else{
+        x1=x2;
+        y1=y2;
+        break;
+      }
+    case "arrowLeft":
+      if(x2>=10){
+        x2 = x2 - distance;
+        drawOneLine(color, x1, y1, x2, y2, area);
+        x1=x2;
+        y1=y2;
+        break;
+      }
+      else{
+        x1=x2;
+        y1=y2;
+        break;
+      }
+    case "arrowRight":
+      if(x2<=290){
+        x2 = x2 + distance;
+        drawOneLine(color, x1, y1, x2, y2, area);
+        x1 = x2;
+        y1 = y2;
+        break;
+      }
+      else{
+        x1=x2;
+        y1=y2;
+        break;
+      }
+    case "resetButton":
+      area.clearRect(0,0,300,300);
+      drawOneLine(color,x1-1,y1-1,x2+1,y2+1,area);
+      break;
+    }
 }
 
-function keyPressed(evento)
-{
+function keyPressed(evento){
   var distance=10;
-  console.log(evento.keyCode);
-  switch (evento.keyCode)
-  {
+  
+  switch (evento.keyCode){
     case arrowKey.UP:
       if (y2>=10){
         y2 = y2 - distance;
